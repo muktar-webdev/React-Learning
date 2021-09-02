492c2690e4225208102ead73834e6782ae250e88
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-02 23:33:26 +0600

    Starting Fragment

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import "./App.css";
-// import Users from "./components/Users";
-// import Child from "./composition/inheritance/Child";
-import Parent from "./composition/compo/Parent";
-import Child from "./composition/compo/Child";
-import Parent1 from "./composition/NestedComposition/Parent1";
-import Parent2 from "./composition/NestedComposition/Parent2";
-import Child1 from "./composition/NestedComposition/Child1";
 
 function App() {
   return (
     <div className="App">
-      {/* --Compositin --// */}
-      <Parent>{({ addEmoji }) => <Child addEmoji={addEmoji}></Child>}</Parent>
-
-      {/* --Nested Composition -- */}
-
-      <Parent1>
-        {({ addEmoji }) => (
-          <Parent2>
-            {({ addBracket }) => (
-              <Child1 addEmoji={addEmoji} addBracket={addBracket}></Child1>
-            )}
-          </Parent2>
-        )}
-      </Parent1>
+     
     </div>
   );
 }
