6b97f08606d17f24a6b588044bec736b85a8f926
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-03 00:31:20 +0600

    Completed React Fragment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Fragment from "./components/Fragment";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Fragment />
+    </>
   );
 }
 
---
+++ src/components/Fragment.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Fragment.css";
+
+const Fragment = () => {
+  return (
+    <React.Fragment>
+      <div className="fragment-container">
+        <div className="div1">
+          <h1>Div 1</h1>
+          <h1>Div 1</h1>
+          <h1>Div 1</h1>
+          <h1>Div 1</h1>
+        </div>
+        <div className="div2">
+          <h1>Div 2</h1>
+          <h1>Div 2</h1>
+          <h1>Div 2</h1>
+          <h1>Div 2</h1>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Fragment;
