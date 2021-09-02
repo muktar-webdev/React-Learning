77d12a99bbd9c37f51b9adff74d9068fb347df71
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-03 00:34:48 +0600

    Starting React Hooks

--- src/composition/NestedComposition/Parent2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Parent2 extends React.Component {
-  addBracket = (text) => ` [${text}]`;
-
-  render() {
-    return this.props.children({ addBracket: this.addBracket });
-  }
-}
