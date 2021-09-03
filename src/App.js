dfdd33a41461be1b8ce4c504c433c9ab0a8b8e3c
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-03 11:29:45 +0600

    Completed useMemo Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import "./App.css";
 import Counter from "./components/USESTATE/Counter";
 import DataLoad from "./components/UseEffect/DataLoad";
@@ -20,6 +20,24 @@ function App() {
   const incrementByFive = useCallback((e) => {
     setCount2((preCount) => preCount + 5);
   }, []);
+
+  // //--Without useMemo --//
+  // const isEvenOrOdd = (e) => {
+  //   let i = 0;
+  //   while (i < 1000000000) i += 1;   //---Costly Operation --//
+  //   return count1 % i === 0;
+  // };
+
+  //--with useMemo--//
+  const isEvenOrOdd = useMemo(
+    (e) => {
+      let i = 0;
+      while (i < 1000000000) i += 1; //---Costly Operation --//
+      return count1 % i === 0;
+    },
+    [count1]
+  );
+
   return (
     <div className="App">
       {/* --useState Hook-- */}
@@ -30,6 +48,9 @@ function App() {
       <Title />
       <ShowCount count={count1} title="Counter 1" />
       <br />
+      {/* //--withoutMemo --//
+      <span>{isEvenOrOdd() ? "Even" : "Odd"}</span> */}
+      <span>{isEvenOrOdd ? "Even" : "Odd"}</span>
       <Button handleClick={incrementByOne}>Increment By One</Button>
       <br />
       <hr />
