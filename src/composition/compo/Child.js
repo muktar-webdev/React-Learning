329fa4d38c6c8f58690d0a199eb77564e809b360
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-09-04 02:05:50 +0600

    Starting Context Api

--- src/composition/compo/Child.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Child = ({ addEmoji }) => {
-  const text = "I'm React Lover.";
-  return <div>{addEmoji ? addEmoji(text, "💋") : text};</div>;
-};
-
-export default Child;
